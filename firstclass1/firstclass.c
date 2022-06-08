39892d4e8fbc18854c45c0e481189c128c7c6d8c
daoson12 <daoduvictor48@gmail.com>
2022-06-08 12:20:03 +0100

    created a file

---
+++ firstclass1/firstclass.c
@@ -0,0 +1,40 @@
+/**
+ * @file firstclass.c
+ * @author victor Daodu (victordaodu48@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+
+/**
+ * include a header file
+ * that defines input/output
+ * routines used by our program.
+ */
+#include <stdio.h>
+
+/**
+ * The entry point
+ * of our program
+ * 
+ * @return int 
+ */
+int main(void){
+
+    /**
+     * Prints some characters
+     * out to standard output
+     */
+    printf("My name is Victor Daodu \n");
+    /**
+     * Program returns the integer 0
+     * to whomever called 
+     * 
+     */
+    return 0;
+}
