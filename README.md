4dec2e74b5e36aad3718185397c73f68cde4f7b9
daoson12 <daoduvictor48@gmail.com>
2022-06-08 15:04:59 +0100

    added an Escape Sequence

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ example: //this is a single line comment
 *comments
 */
 
+ 2. ---Escape Sequence--
+     * @brief
+     * escape sequence = character combination consisting of a backslash \
+     * followed by a letter or combination of digits.
+     * they specify actions within a line or string of text.
+     * \n = newline
+     * \t = tab 
+     *for more knowledge visit this link: https://www.javatpoint.com/escape-sequence-in-c
+     */
---
+++ firstclass1/escapesequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    /**
+     * @brief
+     * escape sequence = character combination consisting of a backslash \
+     * followed by a letter or combination of digits.
+     * they specify actions within a line or string of text.
+     * \n = newline
+     * \t = tab 
+     * https://www.javatpoint.com/escape-sequence-in-c
+     */
+    printf("my name\nis\nvictor daodu\n");
+    printf("1\t2\t3\t4\n");
+    printf("\"I love Pizza \"- Abraham Lincon");
+    return 0;
+
+}
